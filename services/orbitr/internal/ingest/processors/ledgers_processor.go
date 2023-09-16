538ae290f9ff4ed0612a119f6b28b41eac4480bd
Daniel Jeffery <danielhjeffery@gmail.com>
2023-09-15 21:07:35 -0400

    typo

--- services/orbitr/internal/ingest/processors/ledgers_processor.go
+++ services/orbitr/internal/ingest/processors/ledgers_processor.go
@@ -4,7 +4,7 @@ import (
 	"context"
 
 	"github.com/lantah/go/ingest"
-	"github.com/lantah/go/services/orbitrinternal/db2/history"
+	"github.com/lantah/go/services/orbitr/internal/db2/history"
 	"github.com/lantah/go/support/errors"
 	"github.com/lantah/go/xdr"
 )
